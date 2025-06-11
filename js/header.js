9d0f2bb11568b0197bd6e9aa7eda1dfa251fca8f
Nickolay-36-996 <trained.educated879@gmail.com>
2025-06-11 03:31:22 +0300

    message

--- js/header.js
+++ js/header.js
@@ -10,5 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const burgerMobile = document.querySelector(".burger__menu__mobile");
     burgerMobile.addEventListener("click", function() {
         this.classList.toggle("burger__active__mobile");
+        nav.classList.toggle("header__low__nav__active");
     })
 });
