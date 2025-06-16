4dffaa74008b8d2b52215db23e3cc63cc88e8360
Nickolay-36-996 <trained.educated879@gmail.com>
2025-06-16 03:11:21 +0300

    message

--- js/products__catalog.js
+++ js/products__catalog.js
@@ -1,5 +1,14 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', () => {
-    let revealSelect = document.querySelector('.select__icon');
-    let selectList = document.querySelector('.products__catalog__sort__select__list');
+    const revealSelect = document.querySelector('.select__icon');
+    const selectList = document.querySelector('.products__catalog__sort__select__list');
+    revealSelect.addEventListener('click', function() {
+        selectList.classList.toggle('products__catalog__sort__select__list__active');
+        revealSelect.classList.toggle('select__icon__active');
+    })
+
+    const promotionalIndicator = document.querySelector('.promotional__item__indicator');
+    promotionalIndicator.addEventListener('click', function() {
+        promotionalIndicator.classList.toggle('promotional__item__indicator__active');
+    })
 })
